ca5e2088e1cb72154d4ae8f1a39e0a4c2d490c7e
crystal <cristalcruz19@gmail.com>
2024-05-15 23:12:35 -0500

    updated server.js, added unique id parameter npm i ,

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 ## Resources
 
+- Parameter used UniqueID https://www.npmjs.com/package/uniqueid npm I uniqueid
+   https://stackoverflow.com/questions/70700607/how-to-add-a-unique-id-to-each-entry-in-my-json-object
+   
 - getting data from the backend https://youtu.be/C_vv1D5oDZ0?si=1F64f7tUl4ma9nw0v
 - Using __dirname https://www.digitalocean.com/community/tutorials/nodejs-how-to-use__dirname
+- appending json https://www.geeksforgeeks.org/how-to-add-data-in-json-file-using-node-js/
 
--- develop/server.js
+++ develop/server.js
@@ -5,7 +5,7 @@ const fs = require(`fs`);
 const path = require (`path`);
 const uniqid = require (`uniqid`);
 //port
-const port  = process.env.port || 3001;
+const PORT  = process.env.port || 3001;
 
 
 // Create New App
@@ -36,21 +36,80 @@ res.sendFile(path.join(__dirname, `Develop/public/notes.html`))
 
 app.get(`/api/notes`,function (req,res){
     fs.readFile(`Develop/db/db.json`,`utf8`, (err , data) =>{
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeNewNoteToJson (file, parsedData);
-        }
-    });
+       var jsonData = JSON.parse(data);
+       console.log(jsonData);
+       res.json(jsonData);
+        });
+
 });
+
+const readThenAppendJson = (content , file) => {
+    fs.readFile(file, `utf8`, (err, data) =>{
+    if (err) {
+        console.error(err);
+    } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeNewNoteToJson (file, parsedData);
+    }
+    });
+};
 // add a third parameter for spacing on json
 const writeNewNoteToJson = (destinaton, content) =>
     fs.writeFile (destinaton, JSON.stringify(content, null,),(err) =>
         err ? console.error(err): console.info (`\nData ${destinaton}`)
     );
 
+
+    // POST
+
+    app.post(`/api/notes`, (req, res) =>{
+        const {title , text} = req.body;
+        if (title && text){
+            const newNote = {
+                title: title,
+                text : text,
+                // add id third parameter
+            };
+            readThenAppendJson(newNote, `Develop/db/db.json`);
+            const response = {
+                status: 'Note Posted',
+                body: newNote,
+            };
+            res.json(response);
+        } else{ 
+            res.json(`Error posting note`);
+        }
+    }
+);
+//Uses the uniqueid to create the json
+
+app.delete (`/api/notes/:id`,(req,res) =>{
+    let id = req.params.id;
+    let parsedData;
+    fs.readFile(`Develop/db/db.json`, `utf8`, (err,data) =>
+        {
+            if (err){
+                console.log(err)
+
+            } else {
+                parsedData = JSON.parse(data);
+                const filterData = paresedData.filter((note)=> note.id !== id);
+                writeNewNoteToJson(`Develop/db/db.json`, filterData);
+
+            }
+        }
+    );
+    res.send (`Delete note with ${req.params.id}`);
+}
+);
+
+
+
+//App listen for local server
+
+app.listen(PORT, ()=>
+console.log(`App listening http://localhost;${PORT}`));
     
 
 
